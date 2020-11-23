16b5e871872e1c18260d3828f6231bb8118ad844
farther <farther.mail@gmail.com>
2020-11-23 13:29:35 +0200

    async

--- umico-catalog/app/workers/elastic/bulk_update_worker.rb
+++ umico-catalog/app/workers/elastic/bulk_update_worker.rb
@@ -6,13 +6,18 @@ module Elastic
 
     def work(opts)
       data = JSON.parse(opts)
+      model = data['klass'].constantize
 
       ActiveRecord::Base.connection_pool.with_connection do
         ActiveRecord::Base.uncached do
-          records = data['klass'].constantize.where(id: data['record_ids'])
-          return if records.size.nil?
+          objects = model.where(id: data['ids'])
 
-          data['klass'].constantize.bulk_update(records)
+          if model.is_a?(Product)
+            objects = objects.includes(:marketing_name, :product_custom_fields, :product_offers, :translations, categories: :translations)
+          end
+          return if objects.size.nil?
+
+          model.bulk_update(objects)
         end
       end
 
